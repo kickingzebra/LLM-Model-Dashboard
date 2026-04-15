5ac4d4de6ba02594596a28cdda9d93b89aab73a4
Zia Basit <ziabasit@mac.lan>
2026-04-15 18:10:18 +0100

    Improve dashboard browser compatibility

--- src/app.js
+++ src/app.js
@@ -1245,11 +1245,11 @@ function renderDashboardHtml() {
 
     function escapeHtml(value) {
       return String(value)
-        .replaceAll('&', '&amp;')
-        .replaceAll('<', '&lt;')
-        .replaceAll('>', '&gt;')
-        .replaceAll('"', '&quot;')
-        .replaceAll("'", '&#39;');
+        .replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
     }
 
     function renderHealth(health) {
